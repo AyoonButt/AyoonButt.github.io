71b8c95f10a7647207d1c0b0a966c4b7a6bdc8a4
Ayoon Butt <ayoonahmadbutt@gmail.com>
2023-12-31 04:55:42 -0600

    correct client side code

--- authenticate.js
+++ authenticate.js
@@ -2,15 +2,16 @@
 
 function initiateAuthentication() {
     // Make an HTTP request to the server to initiate Twitter authentication
-    axios.get('/initiate-authentication')
-      .then(response => {
-        console.log("Initiating authentication..."); // Add this line
+    axios.get('https://authenthicatebot.azurewebsites.net/initiate-authentication')
+    .then(response => {
+        console.log("Initiating authentication...");
         console.log(response.data);
         // Handle success if needed
-      })
-      .catch(error => {
+    })
+    .catch(error => {
         console.error(error);
         // Handle error if needed
-      });
-  }
+    });
+}
+
   
