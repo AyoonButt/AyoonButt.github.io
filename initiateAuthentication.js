8fd9f321edc14bb2022906dd90e8f261eebeef03
Ayoon Butt <ayoonahmadbutt@gmail.com>
2024-01-06 04:22:26 -0600

    move to server side code

--- initiateAuthentication.js
+++ initiateAuthentication.js
@@ -1,18 +1,11 @@
 // initiateAuthentication.js
 
 function initiateAuthentication() {
-    console.log("Initiating authentication...");
-    
     // Make an HTTP request to the server to initiate Twitter authentication
     axios.get('/initiate-authentication')
-      .then(response => {
-        console.log("Authentication initiated successfully:");
-        console.log(response.data);
-        // Handle success if needed
-      })
       .catch(error => {
-        console.error("Error initiating authentication:", error);
-        // Handle error if needed
+        console.error('Error initiating authentication:', error.message);
+        // Handle the error appropriately
       });
   }
   
