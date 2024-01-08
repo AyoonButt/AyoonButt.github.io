f3171569341c7f2047837dc4d23965556f32adda
Ayoon Butt <ayoonahmadbutt@gmail.com>
2024-01-08 04:16:15 -0600

    change response type

--- initiateAuthentication.js
+++ initiateAuthentication.js
@@ -1,11 +1,10 @@
 // initiateAuthentication.js
 
-function initiateAuthentication() {
-    // Make an HTTP request to the server to initiate Twitter authentication
-    axios.get('/initiate-authentication')
-      .catch(error => {
-        console.error('Error initiating authentication:', error.message);
-        // Handle the error appropriately
-      });
-  }
-  
+axios.get('/initiate-authentication', { responseType: 'text' })
+  .then(response => {
+    // Handle the HTML response here
+    console.log(response.data);
+  })
+  .catch(error => {
+    // Handle errors as needed
+  });
