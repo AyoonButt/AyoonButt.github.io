e2766d3d42d7e2b987124060189288dd64ef38de
Ayoon Butt <ayoonahmadbutt@gmail.com>
2024-01-19 23:54:59 -0600

    update client side

--- initiateAuthentication.js
+++ initiateAuthentication.js
@@ -3,6 +3,7 @@
 function initiateAuthentication(event) {
   event.preventDefault();
   axios.get('/initiate-authentication/')
+    
     .then(response => {
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
