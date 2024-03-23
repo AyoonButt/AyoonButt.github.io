909ca5819a87ab1bb9175c0074216367c10fcf5f
Ayoon Butt <111302380+AyoonButt@users.noreply.github.com>
2024-03-23 17:34:09 -0500

    have the server handle redirect

--- server/api/initiate-authentication/initiate-authentication.js
+++ server/api/initiate-authentication/initiate-authentication.js
@@ -25,7 +25,8 @@ apiRouter.get('/initiate-authentication', async (req, res) => {
 
     const twitterAuthUrl = `https://api.twitter.com/oauth/authenticate?client_id=${config.twitterApi.apiKey}&redirect_uri=https://authenthicatebot.azurewebsites.net/callback&response_type=code&scope=read&code_challenge=${codeChallenge}&code_challenge_method=S256&state=${state}`;
 
-    res.json({ redirectUrl: twitterAuthUrl });
+    // Redirect the browser to Twitter's authorization page
+    res.redirect(302, twitterAuthUrl);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
